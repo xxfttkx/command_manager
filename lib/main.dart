c4fc4e68661d24b72d3c5097a11f4afdb7ebc171
lauirf <592184299@qq.com>
2025-07-22 14:38:43 +0800

    feat: add system_tray(cloe #1)

--- lib/main.dart
+++ lib/main.dart
@@ -8,9 +8,47 @@ import 'package:command_manager/viewmodels/locale_viewmodel.dart';
 import 'package:command_manager/viewmodels/theme_viewmodel.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'dart:io';
+import 'package:system_tray/system_tray.dart';
+
+Future<void> initSystemTray() async {
+  String path =
+      Platform.isWindows ? 'assets/app_icon.ico' : 'assets/app_icon.png';
+
+  final AppWindow appWindow = AppWindow();
+  final SystemTray systemTray = SystemTray();
+
+  // We first init the systray menu
+  await systemTray.initSystemTray(
+    title: "system tray",
+    iconPath: path,
+  );
+
+  // create context menu
+  final Menu menu = Menu();
+  await menu.buildFrom([
+    MenuItemLabel(label: 'Show', onClicked: (menuItem) => appWindow.show()),
+    MenuItemLabel(label: 'Hide', onClicked: (menuItem) => appWindow.hide()),
+    MenuItemLabel(label: 'Exit', onClicked: (menuItem) => appWindow.close()),
+  ]);
+
+  // set context menu
+  await systemTray.setContextMenu(menu);
+
+  // handle system tray event
+  systemTray.registerSystemTrayEventHandler((eventName) {
+    debugPrint("eventName: $eventName");
+    if (eventName == kSystemTrayEventClick) {
+      Platform.isWindows ? appWindow.show() : systemTray.popUpContextMenu();
+    } else if (eventName == kSystemTrayEventRightClick) {
+      Platform.isWindows ? systemTray.popUpContextMenu() : appWindow.show();
+    }
+  });
+}
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await initSystemTray();
 
   final themeViewModel = ThemeViewModel();
   final localeViewModel = LocaleViewModel();
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,8 +7,10 @@ import Foundation
 
 import path_provider_foundation
 import shared_preferences_foundation
+import system_tray
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  SystemTrayPlugin.register(with: registry.registrar(forPlugin: "SystemTrayPlugin"))
 }
